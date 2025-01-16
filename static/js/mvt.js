fd1c9218eec65508813ea2774f231d1156da555a
Jose Jachuf <jjachuf@gmail.com>
2025-01-15 23:40:10 -0300

    refactoring

--- static/js/mvt.js
+++ static/js/mvt.js
@@ -1,1 +1,20 @@
 console.log("mvt-server");
+
+function initializePage() {
+  let protocol = window.location.protocol;
+  let host = window.location.hostname;
+  let port = window.location.port;
+
+  if (port === '') {
+    port = protocol === 'https:' ? '443' : '80';
+  }
+
+  let servers = document.getElementsByClassName("server");
+  for (let i = 0; i < servers.length; i++) {
+    let serverAddress = `${protocol}//${host}`;
+    if (port !== '80' && port !== '443') {
+      serverAddress += `:${port}`;
+    }
+    servers[i].textContent = serverAddress;
+  }
+}
