04332bc374d36774fa99b9878f067e139b19acd3
Jose jachuf <jjachuf@gmail.com>
2025-01-17 09:17:29 -0300

    upgrade salvo to 0.76, fix args routes

--- src/routes.rs
+++ src/routes.rs
@@ -61,7 +61,7 @@ pub fn app_router() -> salvo::Router {
         )
         .push(Router::with_path("catalog").get(html::main::page_catalog))
         .push(Router::with_path("styles").get(html::main::page_styles))
-        .push(Router::with_path("map/<layer_name>").get(html::main::page_map))
+        .push(Router::with_path("map/{layer_name}").get(html::main::page_map))
         .push(Router::with_path("health").get(health::get_health))
         .push(
             Router::with_path("admin")
@@ -73,10 +73,10 @@ pub fn app_router() -> salvo::Router {
                         .get(html::admin::users::list_users)
                         .push(Router::with_path("new").get(html::admin::main::new_user))
                         .push(Router::with_path("create").post(html::admin::users::create_user))
-                        .push(Router::with_path("edit/<id>").get(html::admin::main::edit_user))
+                        .push(Router::with_path("edit/{id}").get(html::admin::main::edit_user))
                         .push(Router::with_path("update").post(html::admin::users::update_user))
                         .push(
-                            Router::with_path("delete/<id>").get(html::admin::users::delete_user),
+                            Router::with_path("delete/{id}").get(html::admin::users::delete_user),
                         ),
                 )
                 .push(
@@ -88,13 +88,13 @@ pub fn app_router() -> salvo::Router {
                             Router::with_path("create")
                                 .post(html::admin::categories::create_category),
                         )
-                        .push(Router::with_path("edit/<id>").get(html::admin::main::edit_category))
+                        .push(Router::with_path("edit/{id}").get(html::admin::main::edit_category))
                         .push(
                             Router::with_path("update")
                                 .post(html::admin::categories::edit_category),
                         )
                         .push(
-                            Router::with_path("delete/<id>")
+                            Router::with_path("delete/{id}")
                                 .get(html::admin::categories::delete_category),
                         ),
                 )
@@ -104,10 +104,10 @@ pub fn app_router() -> salvo::Router {
                         .get(html::admin::styles::list_styles)
                         .push(Router::with_path("new").get(html::admin::main::new_style))
                         .push(Router::with_path("create").post(html::admin::styles::create_style))
-                        .push(Router::with_path("edit/<id>").get(html::admin::main::edit_style))
+                        .push(Router::with_path("edit/{id}").get(html::admin::main::edit_style))
                         .push(Router::with_path("update").post(html::admin::styles::edit_style))
                         .push(
-                            Router::with_path("delete/<id>").get(html::admin::styles::delete_style),
+                            Router::with_path("delete/{id}").get(html::admin::styles::delete_style),
                         ),
                 )
                 .push(
@@ -116,10 +116,10 @@ pub fn app_router() -> salvo::Router {
                         .get(html::admin::groups::list_groups)
                         .push(Router::with_path("new").get(html::admin::main::new_group))
                         .push(Router::with_path("create").post(html::admin::groups::create_group))
-                        .push(Router::with_path("edit/<id>").get(html::admin::main::edit_group))
+                        .push(Router::with_path("edit/{id}").get(html::admin::main::edit_group))
                         .push(Router::with_path("update").post(html::admin::groups::edit_group))
                         .push(
-                            Router::with_path("delete/<id>").get(html::admin::groups::delete_group),
+                            Router::with_path("delete/{id}").get(html::admin::groups::delete_group),
                         ),
                 )
                 .push(
@@ -132,11 +132,11 @@ pub fn app_router() -> salvo::Router {
                                 .post(html::admin::catalog::create_layer),
                         )
                         .push(
-                            Router::with_path("layers/edit/<id>")
+                            Router::with_path("layers/edit/{id}")
                                 .get(html::admin::main::edit_layer),
                         )
                         .push(
-                            Router::with_path("layers/delete/<id>")
+                            Router::with_path("layers/delete/{id}")
                                 .get(html::admin::catalog::delete_layer),
                         )
                         .push(
@@ -144,7 +144,7 @@ pub fn app_router() -> salvo::Router {
                                 .post(html::admin::catalog::update_layer),
                         )
                         .push(
-                            Router::with_path("layers/swichpublished/<id>")
+                            Router::with_path("layers/swichpublished/{id}")
                                 .get(html::admin::catalog::swich_published),
                         ),
                 )
@@ -179,14 +179,14 @@ pub fn app_router() -> salvo::Router {
                                 .hoop(auth::validate_token)
                                 .push(Router::with_path("schemas").get(api::database::schemas))
                                 .push(
-                                    Router::with_path("tables/<schema>").get(api::database::tables),
+                                    Router::with_path("tables/{schema}").get(api::database::tables),
                                 )
                                 .push(
-                                    Router::with_path("fields/<schema>/<table>")
+                                    Router::with_path("fields/{schema}/{table}")
                                         .get(api::database::fields),
                                 )
                                 .push(
-                                    Router::with_path("srid/<schema>/<table>/<geometry>")
+                                    Router::with_path("srid/{schema}/{table}/{geometry}")
                                         .get(api::database::srid),
                                 ),
                         )
@@ -197,7 +197,7 @@ pub fn app_router() -> salvo::Router {
                                 .post(api::catalog::create_layer),
                         )
                         .push(
-                            Router::with_path("<**rest>")
+                            Router::with_path("{**rest}")
                                 .hoop(cors_handler.clone())
                                 .options(handler::empty()),
                         ),
@@ -209,8 +209,8 @@ pub fn app_router() -> salvo::Router {
                 .hoop(cors_handler)
                 .options(handler::empty())
                 .push(Router::with_path("tiles").get(tiles::mvt))
-                .push(Router::with_path("tiles/<layer_name>/<z>/<x>/<y>.pbf").get(tiles::mvt))
-                .push(Router::with_path("styles/<style_name>").get(styles::index)),
+                .push(Router::with_path("tiles/{layer_name}/{z}/{x}/{y}.pbf").get(tiles::mvt))
+                .push(Router::with_path("styles/{style_name}").get(styles::index)),
         )
-        .push(Router::with_path("static/<**path>").get(static_dir))
+        .push(Router::with_path("static/{**path}").get(static_dir))
 }
